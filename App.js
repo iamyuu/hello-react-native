f7dc004aaa1b0babfbf2838539a8edcd341fda49
iamyuu <akuyusuf27@gmail.com>
2020-11-17 13:31:07 +0700

    style: title bar & learning section

--- App.js
+++ App.js
@@ -1,21 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
+import styled from 'styled-components/native';
+import Card from './components/card';
+import { NotificationIcon } from './components/icon';
+
+const cardItems = [
+  {
+    title: 'Styled Components',
+    caption: 'React Native',
+    subTitle: '5 of 12 sections',
+    logo: require('./assets/logo-react.png'),
+    background: require('./assets/background2.jpg')
+  },
+  {
+    title: 'Styled Components 2',
+    caption: 'React Native',
+    subTitle: '5 of 12 sections',
+    logo: require('./assets/logo-react.png'),
+    background: require('./assets/background1.jpg')
+  }
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <SafeAreaView>
+        <ScrollView>
+          <TitleBar>
+            <Avatar source={require('./assets/avatar.jpg')} />
+            <Title>Welcome back,</Title>
+            <Name>iamyuu</Name>
+            <NotificationIcon style={{ position: 'absolute', right: 20, top: 0 }} />
+          </TitleBar>
+
+          <Subtitle>Continue Learning</Subtitle>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {cardItems.map((cardItem, index) => (
+              <Card key={index} {...cardItem} />
+            ))}
+          </ScrollView>
+        </ScrollView>
+      </SafeAreaView>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  background: #f0f3f5;
+  flex: 1;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
---
+++ components/card.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export default function Card({ title, caption, subTitle, logo, background }) {
+  return (
+    <Container>
+      <Cover>
+        <Image source={background} />
+        <Title>{title}</Title>
+      </Cover>
+      <Content>
+        <Logo source={logo} />
+        <Wrapper>
+          <Caption>{caption}</Caption>
+          <Subtitle>{subTitle}</Subtitle>
+        </Wrapper>
+      </Content>
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  background-color: white;
+  width: 315px;
+  height: 280px;
+  border-radius: 14px;
+  margin: 20px 10px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
+`;
+
+const Cover = styled.View`
+  width: 100%;
+  height: 200px;
+  border-top-left-radius: 14px;
+  border-top-right-radius: 14px;
+  overflow: hidden;
+`;
+
+const Image = styled.Image`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  width: 170px;
+  margin-top: 20px;
+  margin-left: 20px;
+`;
+
+const Content = styled.View`
+  padding-left: 20px;
+  flex-direction: row;
+  align-items: center;
+  height: 80px;
+`;
+
+const Logo = styled.Image`
+  width: 44px;
+  height: 44px;
+`;
+
+const Wrapper = styled.View`
+  margin-left: 10px;
+`;
+
+const Caption = styled.Text`
+  color: #3c4560;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-size: 15px;
+  font-weight: 600;
+  text-transform: uppercase;
+  margin-top: 4px;
+`;
